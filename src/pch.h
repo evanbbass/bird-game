9479eca036f3a47aee271d9009352e6800c1a784
Evan Bass <evanbbass@gmail.com>
2025-01-08 13:18:18 -0500

    Add some DirectX files to pch

--- src/pch.h
+++ src/pch.h
@@ -3,6 +3,12 @@
 // Windows
 #include <Windows.h>
 
+// DirectX
+#include <directx/d3d12.h> // This helper library has to be included before any SDK headers
+#include <d3d12.h>
+#include <d3dcompiler.h>
+#include <DirectXMath.h>
+
 // Standard libraries
 #include <cstdint>
 #include <memory>
