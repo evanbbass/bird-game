afbe7b79f19c7e8f30219881d8214b045b72fe5b
Evan Bass <evanbbass@gmail.com>
2025-01-08 13:48:47 -0500

    Add interface for RendererImpl

--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -1,12 +1,36 @@
 #include "pch.h"
 #include "Renderer.h"
 
-
 namespace BirdGame
 {
 	class RendererImpl
 	{
+	public:
+		RendererImpl() = default;
+		~RendererImpl() = default;
 
+		void CreateDevice();
+		void CreateCommandQueue();
+		void CreateSwapChain(HWND hwnd, uint32_t width, uint32_t height);
+		void CreateCommandList();
+		void CreateFence();
+
+		void InitializeTriangleRenderer();
+
+		void PopulateCommandListAndSubmit();
+		void Present();
+		void WaitForPreviousFram();
+
+		void AddDebugText(std::string_view text, int32_t x, int32_t y);
+
+	private:
+		ID3D12Device* mDevice;
+		ID3D12CommandQueue* mCommandQueue;
+		//IDXGISwapChain3* mSwapChain;
+		ID3D12CommandAllocator* mCommandAllocator;
+
+		uint32_t mWidth;
+		uint32_t mHeight;
 	};
 }
 
