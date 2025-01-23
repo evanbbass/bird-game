36b58cf3670c4277b034f42f55c8d5c522490840
Evan Bass <evanbbass@gmail.com>
2025-01-23 15:01:03 -0500

    Add triangle rendering

--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -31,6 +31,13 @@ namespace BirdGame
 	class RendererImpl final
 	{
 	public:
+		struct Vertex
+		{
+			DirectX::XMFLOAT3 position;
+			DirectX::XMFLOAT4 color;
+		};
+
+	public:
 		RendererImpl();
 		~RendererImpl();
 
@@ -49,23 +56,25 @@ namespace BirdGame
 		void Destroy();
 
 	private:
-
+		void Initialize(uint32_t width, uint32_t height);
 		void CreateDevice();
 		void CreateCommandQueue();
-		void CreateSwapChain(HWND hwnd, uint32_t width, uint32_t height);
+		void CreateSwapChain(HWND hwnd);
 
-		void CreateCommandList();
 		void LoadShaders();
+		void CreateCommandList();
+		void CreateVertexBuffer();
 		void CreateFence();
 
-		uint32_t mWidth;
-		uint32_t mHeight;
-		float mAspectRatio;
+		CD3DX12_VIEWPORT mViewport;
+		CD3DX12_RECT mScissorRect;
 
 		ComPtr<ID3D12Device> mDevice;
 		ComPtr<ID3D12CommandQueue> mCommandQueue;
 		ComPtr<IDXGISwapChain3> mSwapChain;
 		ComPtr<ID3D12CommandAllocator> mCommandAllocator;
+
+		ComPtr<ID3D12RootSignature> mRootSignature;
 
 		ComPtr<ID3D12DescriptorHeap> mRtvHeap;
 		uint32_t mRtvDescriptorSize;
@@ -75,6 +84,10 @@ namespace BirdGame
 
 		ComPtr<ID3D12PipelineState> mPipelineState;
 
+		// App resources.
+		ComPtr<ID3D12Resource> mVertexBuffer;
+		D3D12_VERTEX_BUFFER_VIEW mVertexBufferView;
+
 		uint32_t mFrameIndex;
 		ComPtr<ID3D12Fence> mFence;
 		HANDLE mFenceEvent;
@@ -83,8 +96,6 @@ namespace BirdGame
 }
 
 BirdGame::RendererImpl::RendererImpl() :
-	mWidth(0), mHeight(0),
-	mAspectRatio(0.0f),
 	mRtvDescriptorSize(0),
 	mFrameIndex(0),
 	mFenceEvent(NULL),
@@ -101,15 +112,17 @@ BirdGame::RendererImpl::~RendererImpl()
 
 void BirdGame::RendererImpl::LoadPipeline(HWND hwnd, uint32_t width, uint32_t height)
 {
+	Initialize(width, height);
 	CreateDevice();
 	CreateCommandQueue();
-	CreateSwapChain(hwnd, width, height);
+	CreateSwapChain(hwnd);
 }
 
 void BirdGame::RendererImpl::LoadAssets()
 {
-	CreateCommandList();
 	LoadShaders();
+	CreateCommandList();
+	CreateVertexBuffer(); // Set up the vertex buffers here for now since this shader is very basic and not doing anything interesting
 	CreateFence();
 }
 
@@ -125,14 +138,23 @@ void BirdGame::RendererImpl::PopulateCommandList()
 	// re-recording.
 	CheckHResult(mCommandList->Reset(mCommandAllocator.Get(), mPipelineState.Get()));
 
+	// Set necessary state.
+	mCommandList->SetGraphicsRootSignature(mRootSignature.Get());
+	mCommandList->RSSetViewports(1, &mViewport);
+	mCommandList->RSSetScissorRects(1, &mScissorRect);
+
 	// Indicate that the back buffer will be used as a render target.
 	mCommandList->ResourceBarrier(1, &CD3DX12_RESOURCE_BARRIER::Transition(mRenderTargets[mFrameIndex].Get(), D3D12_RESOURCE_STATE_PRESENT, D3D12_RESOURCE_STATE_RENDER_TARGET));
 
 	CD3DX12_CPU_DESCRIPTOR_HANDLE rtvHandle(mRtvHeap->GetCPUDescriptorHandleForHeapStart(), mFrameIndex, mRtvDescriptorSize);
+	mCommandList->OMSetRenderTargets(1, &rtvHandle, FALSE, nullptr);
 
 	// Record commands.
 	const float clearColor[] = { 0.0f, 0.2f, 0.4f, 1.0f };
 	mCommandList->ClearRenderTargetView(rtvHandle, clearColor, 0, nullptr);
+	mCommandList->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+	mCommandList->IASetVertexBuffers(0, 1, &mVertexBufferView);
+	mCommandList->DrawInstanced(3, 1, 0, 0);
 
 	// Indicate that the back buffer will now be used to present.
 	mCommandList->ResourceBarrier(1, &CD3DX12_RESOURCE_BARRIER::Transition(mRenderTargets[mFrameIndex].Get(), D3D12_RESOURCE_STATE_RENDER_TARGET, D3D12_RESOURCE_STATE_PRESENT));
@@ -184,6 +206,23 @@ void BirdGame::RendererImpl::Destroy()
 	mFenceEvent = NULL;
 }
 
+void BirdGame::RendererImpl::Initialize(uint32_t width, uint32_t height)
+{
+	// Initialize Viewport
+	mViewport.TopLeftX = 0.0f;
+	mViewport.TopLeftY = 0.0f;
+	mViewport.Width = static_cast<float>(width);
+	mViewport.Height = static_cast<float>(height);
+	mViewport.MinDepth = D3D12_MIN_DEPTH;
+	mViewport.MaxDepth = D3D12_MAX_DEPTH;
+
+	// Initialize ScissorRect
+	mScissorRect.left = 0;
+	mScissorRect.top = 0;
+	mScissorRect.right = static_cast<LONG>(width);
+	mScissorRect.bottom = static_cast<LONG>(height);
+}
+
 void BirdGame::RendererImpl::CreateDevice()
 {
 	UINT dxgiFactoryFlags = 0;
@@ -225,12 +264,8 @@ void BirdGame::RendererImpl::CreateCommandQueue()
 	CheckHResult(mDevice->CreateCommandQueue(&queueDesc, IID_PPV_ARGS(&mCommandQueue)));
 }
 
-void BirdGame::RendererImpl::CreateSwapChain(HWND hwnd, uint32_t width, uint32_t height)
+void BirdGame::RendererImpl::CreateSwapChain(HWND hwnd)
 {
-	mWidth = width;
-	mHeight = height;
-	mAspectRatio = static_cast<float>(width) / static_cast<float>(height);
-
 	// Create the swap chain
 	{
 		ComPtr<IDXGIFactory4> factory;
@@ -239,8 +274,8 @@ void BirdGame::RendererImpl::CreateSwapChain(HWND hwnd, uint32_t width, uint32_t
 		// Describe and create the swap chain.
 		DXGI_SWAP_CHAIN_DESC1 swapChainDesc = { 0 };
 		swapChainDesc.BufferCount = kNumBufferFrames;
-		swapChainDesc.Width = mWidth;
-		swapChainDesc.Height = mHeight;
+		swapChainDesc.Width = static_cast<UINT>(mViewport.Width);
+		swapChainDesc.Height = static_cast<UINT>(mViewport.Height);
 		swapChainDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
 		swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
 		swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
@@ -292,6 +327,63 @@ void BirdGame::RendererImpl::CreateSwapChain(HWND hwnd, uint32_t width, uint32_t
 	CheckHResult(mDevice->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&mCommandAllocator)));
 }
 
+void BirdGame::RendererImpl::LoadShaders()
+{
+	// Create an empty root signature.
+	{
+		CD3DX12_ROOT_SIGNATURE_DESC rootSignatureDesc;
+		rootSignatureDesc.Init(0, nullptr, 0, nullptr, D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT);
+
+		ComPtr<ID3DBlob> signature;
+		ComPtr<ID3DBlob> error;
+		CheckHResult(D3D12SerializeRootSignature(&rootSignatureDesc, D3D_ROOT_SIGNATURE_VERSION_1, &signature, &error));
+		CheckHResult(mDevice->CreateRootSignature(0, signature->GetBufferPointer(), signature->GetBufferSize(), IID_PPV_ARGS(&mRootSignature)));
+	}
+
+	// Create the pipeline state, which includes compiling and loading shaders.
+	{
+		ComPtr<ID3DBlob> vertexShader;
+		ComPtr<ID3DBlob> pixelShader;
+
+#if defined(_DEBUG)
+		// Enable better shader debugging with the graphics debugging tools.
+		UINT compileFlags = D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION;
+#else
+		UINT compileFlags = 0;
+#endif
+
+		// NOTE Working Directory needs to be set in project configuration. Since this is stored in the .vxproj.user file,
+		// SharpMake needs special steps to set this correctly. Look into how to do it automatically, but for now remember to go set it :)
+		CheckHResult(D3DCompileFromFile(L"assets/shaders/shaders.hlsl", nullptr, nullptr, "VSMain", "vs_5_0", compileFlags, 0, &vertexShader, nullptr));
+		CheckHResult(D3DCompileFromFile(L"assets/shaders/shaders.hlsl", nullptr, nullptr, "PSMain", "ps_5_0", compileFlags, 0, &pixelShader, nullptr));
+
+		// Define the vertex input layout.
+		D3D12_INPUT_ELEMENT_DESC inputElementDescs[] =
+		{
+			{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 },
+			{ "COLOR", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 12, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 }
+		};
+
+		// Describe and create the graphics pipeline state object (PSO).
+		D3D12_GRAPHICS_PIPELINE_STATE_DESC psoDesc = {};
+		psoDesc.InputLayout = { inputElementDescs, _countof(inputElementDescs) };
+		psoDesc.pRootSignature = mRootSignature.Get();
+		psoDesc.VS = CD3DX12_SHADER_BYTECODE(vertexShader.Get());
+		psoDesc.PS = CD3DX12_SHADER_BYTECODE(pixelShader.Get());
+		psoDesc.RasterizerState = CD3DX12_RASTERIZER_DESC(D3D12_DEFAULT);
+		psoDesc.BlendState = CD3DX12_BLEND_DESC(D3D12_DEFAULT);
+		psoDesc.DepthStencilState.DepthEnable = FALSE;
+		psoDesc.DepthStencilState.StencilEnable = FALSE;
+		psoDesc.SampleMask = UINT_MAX;
+		psoDesc.PrimitiveTopologyType = D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
+		psoDesc.NumRenderTargets = 1;
+		psoDesc.RTVFormats[0] = DXGI_FORMAT_R8G8B8A8_UNORM;
+		psoDesc.SampleDesc.Count = 1;
+
+		CheckHResult(mDevice->CreateGraphicsPipelineState(&psoDesc, IID_PPV_ARGS(&mPipelineState)));
+	}
+}
+
 void BirdGame::RendererImpl::CreateCommandList()
 {
 	// Create the command list.
@@ -302,14 +394,48 @@ void BirdGame::RendererImpl::CreateCommandList()
 	CheckHResult(mCommandList->Close());
 }
 
-void BirdGame::RendererImpl::LoadShaders()
+void BirdGame::RendererImpl::CreateVertexBuffer()
 {
-	// TODO
+	// Define the geometry for a triangle.
+	float aspectRatio = mViewport.Width / mViewport.Height;
+	Vertex triangleVertices[] =
+	{
+		{ { 0.0f, 0.25f * aspectRatio, 0.0f }, { 1.0f, 0.0f, 0.0f, 1.0f } },
+		{ { 0.25f, -0.25f * aspectRatio, 0.0f }, { 0.0f, 1.0f, 0.0f, 1.0f } },
+		{ { -0.25f, -0.25f * aspectRatio, 0.0f }, { 0.0f, 0.0f, 1.0f, 1.0f } }
+	};
+
+	const UINT vertexBufferSize = sizeof(triangleVertices);
+
+	// Note: using upload heaps to transfer static data like vert buffers is not 
+	// recommended. Every time the GPU needs it, the upload heap will be marshalled 
+	// over. Please read up on Default Heap usage. An upload heap is used here for 
+	// code simplicity and because there are very few verts to actually transfer.
+	CheckHResult(mDevice->CreateCommittedResource(
+		&CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD),
+		D3D12_HEAP_FLAG_NONE,
+		&CD3DX12_RESOURCE_DESC::Buffer(vertexBufferSize),
+		D3D12_RESOURCE_STATE_GENERIC_READ,
+		nullptr,
+		IID_PPV_ARGS(&mVertexBuffer)));
+
+	// Copy the triangle data to the vertex buffer.
+	UINT8* pVertexDataBegin;
+	CD3DX12_RANGE readRange(0, 0);        // We do not intend to read from this resource on the CPU.
+	CheckHResult(mVertexBuffer->Map(0, &readRange, reinterpret_cast<void**>(&pVertexDataBegin)));
+	memcpy(pVertexDataBegin, triangleVertices, sizeof(triangleVertices));
+	mVertexBuffer->Unmap(0, nullptr);
+
+	// Initialize the vertex buffer view.
+	mVertexBufferView.BufferLocation = mVertexBuffer->GetGPUVirtualAddress();
+	mVertexBufferView.StrideInBytes = sizeof(Vertex);
+	mVertexBufferView.SizeInBytes = vertexBufferSize;
 }
 
 // A fence is a synchronization primitive that we can use to signal that the GPU is done rendering a frame
 void BirdGame::RendererImpl::CreateFence()
 {
+	// Create synchronization objects and wait until assets have been uploaded to the GPU.
 	CheckHResult(mDevice->CreateFence(0, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&mFence)));
 	mFenceValue = 1;
 
@@ -338,7 +464,9 @@ void BirdGame::Renderer::Initialize(Window& window)
 	mImpl->LoadPipeline(window.GetHandle(), window.GetWidth(), window.GetHeight());
 	mImpl->LoadAssets();
 
-	// Wait for all the setup work we just did to complete because we are going to re-use the command list
+	// Wait for the command list to execute; we are reusing the same command 
+	// list in our main loop but for now, we just want to wait for setup to 
+	// complete before continuing.
 	mImpl->WaitForPreviousFrame();
 }
 
